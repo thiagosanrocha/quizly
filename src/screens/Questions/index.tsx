94717c0412abe7e7f0c65e4e067aa295580261b6
Thiago Rocha <thiagorocha010@gmail.com>
2021-02-11 20:58:12 -0300

    chore: add modal to the questions screen

--- src/screens/Questions/index.tsx
+++ src/screens/Questions/index.tsx
@@ -1,47 +1,70 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import FeatherIcons from 'react-native-vector-icons/Feather';
 
 import Answer from '../../components/Answer';
 import Button from '../../components/Button';
+import Modal from '../../components/Modal';
 
 import * as S from './styles';
 
 const Questions = () => {
+  const [modalIsVisibel, setModalIsVisibel] = useState(false);
+
+  const handleModalClosing = useCallback(
+    () => setModalIsVisibel(!modalIsVisibel),
+    [setModalIsVisibel, modalIsVisibel],
+  );
+
+  const handleModalConfirmation = useCallback(
+    () => console.log('Confirmou'),
+    [],
+  );
+
   return (
-    <S.Container>
-      <S.Header>
-        <S.Status>
-          <S.CurrentQuestion>Question 4 / 15</S.CurrentQuestion>
-          <S.Time>6:34</S.Time>
-        </S.Status>
+    <>
+      <Modal
+        isVisible={modalIsVisibel}
+        title="Your time is not over."
+        description="Are you sure you want to finish?"
+        closeModal={handleModalClosing}
+        confirmModal={handleModalConfirmation}
+      />
 
-        <S.ProgressiveBar>
-          <S.StatusBar value={60} />
-        </S.ProgressiveBar>
-      </S.Header>
+      <S.Container>
+        <S.Header>
+          <S.Status>
+            <S.CurrentQuestion>Question 4 / 15</S.CurrentQuestion>
+            <S.Time>6:34</S.Time>
+          </S.Status>
 
-      <S.CardQuestions>
-        <S.QuestionTitle>
-          In what year was the first Transformers movie released?
-        </S.QuestionTitle>
+          <S.ProgressiveBar>
+            <S.StatusBar value={60} />
+          </S.ProgressiveBar>
+        </S.Header>
 
-        <S.ContainerAnswer>
-          <Answer>2007</Answer>
-          <Answer>1986</Answer>
-          <Answer isFocus>1984</Answer>
-          <Answer>2009</Answer>
-        </S.ContainerAnswer>
+        <S.CardQuestions>
+          <S.QuestionTitle>
+            In what year was the first Transformers movie released?
+          </S.QuestionTitle>
 
-        <S.Footer>
-          <S.ButtonBack>
-            <FeatherIcons name="arrow-left" size={20} color="#FCA82F" />
-            <S.ButtonBackText>Back</S.ButtonBackText>
-          </S.ButtonBack>
+          <S.ContainerAnswer>
+            <Answer>2007</Answer>
+            <Answer>1986</Answer>
+            <Answer isFocus>1984</Answer>
+            <Answer>2009</Answer>
+          </S.ContainerAnswer>
 
-          <Button variant="secondary">Next</Button>
-        </S.Footer>
-      </S.CardQuestions>
-    </S.Container>
+          <S.Footer>
+            <S.ButtonBack>
+              <FeatherIcons name="arrow-left" size={20} color="#FCA82F" />
+              <S.ButtonBackText>Back</S.ButtonBackText>
+            </S.ButtonBack>
+
+            <Button variant="secondary">Next</Button>
+          </S.Footer>
+        </S.CardQuestions>
+      </S.Container>
+    </>
   );
 };
 
